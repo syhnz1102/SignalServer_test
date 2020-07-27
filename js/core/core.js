93a894e32572b71571830fa780e3157943d5202a
jungdw0624 <jdw8998@daum.net>
2020-07-27 13:02:30 +0900

    updated core.exitRoom

--- js/core/core.js
+++ js/core/core.js
@@ -94,3 +94,63 @@ exports.roomJoin = async (socketIo, socket, redisInfo, reqData) => {
         resolve(roomJoinMsg);
     })
 }
+
+exports.exitRoom = async (socketIo, socket, redisInfo, reqData) => {
+    return new Promise((resolve, reject) => {
+        //socket id
+        let socketId = socket.id;
+    
+        //유저 정보
+        let userData;
+
+        //방 정보
+        let roomData;
+        
+        //socket id로 user 정보 가져오기
+        userData = await syncFn.getUserInfoBySocketId(redisInfo, socketId).catch(err => {
+            logger.error(`[ ## SYNC > SIGNAL ### ] getUserInfoBySocketId Error ${err}`);
+        });
+        
+        //유저 정보가 없을 시 바로 종료
+        if(!userData){
+            logger.info(`[ ## SYNC > SIGNAL ### ] There is no such user in Sync Server`);
+            resolve(false);
+            return;
+        }
+
+        //방 정보 가져오기
+        roomData = await syncFn.getRoomDetail(redisInfo, data.roomId).catch(err => {
+            logger.info(`[ ## SYNC > SIGNAL ### ] getRoomDetail Error ${err}`);
+        });
+
+        //방 정보가 없을 시 바로 종료
+        if(!roomData){
+            logger.info(`[ ## SYNC > SIGNAL ### ] There is no such room in Sync Server`);
+            resolve(false);
+            return;
+        }
+
+        delete userData.roomInfo[data.roomId];
+
+        socket.leave(data.roomId);
+
+        //인원 수 체크 해서 sync server에서 room 정보 지우기
+        let userCount = 0;
+        if(socketIo.adapter.rooms[data.roomId]){
+            userCount = socketIo.adapter.rooms[data.roomId].length;
+
+            //아무도 없으면 방 삭제
+            if(userCount == 0){
+                await syncFn.delRoom(redisInfo, roomData.roomId).catch(err => {
+                    logger.error(`[ ## SYNC > SIGNAL ### ] delRoom Error ${err}`);
+                });
+            }
+        } 
+
+        await syncFn.setUserInfoBySocketId(redisInfo, socketId, userData).catch(err => {
+            logger.error(`[ ## SYNC > SIGNAL ### ] setUserInfoBySocketId Error ${err}`);
+        });
+
+        resolve(true);
+    })
+}
